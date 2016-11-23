2d6779f5aac22eda1ead89e793602642920a65d7
Phan Ngoc Lan <phan.ngoclan58@gmail.com>
2016-11-23 15:51:33 +0700

    Added comments

--- Data/datagen.py
+++ Data/datagen.py
@@ -6,15 +6,16 @@ import math
 print('Loading from process.txt...')
 M = np.loadtxt('process.txt',delimiter=' ')
 print('Done!')
-np.random.shuffle(M)
+np.random.shuffle(M) # Tron ngau nhien cac hang trg M
 rows = np.size(M,0)
 
-r1 = math.ceil(float(rows)*0.7)
+r1 = math.ceil(float(rows)*0.7) # Tinh so hang cua tap train
 Train = M[0:r1,:]
-r2 = math.floor(float(rows-r1)*0.5)
+r2 = math.floor(float(rows-r1)*0.5) # Tinh so hang cua tap cv & test
 Cv = M[r1:r1+r2,:]
 Test = M[r1+r2:rows,:]
 
+# Luu vao file
 print('Generating data...')
 np.savetxt('train.txt',Train,delimiter=' ')
 np.savetxt('cv.txt',Cv,delimiter=' ')
